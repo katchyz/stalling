9b2b700c2801dd19c4fea588b028ee2fbf6aabf9
katchyz <katchyz@gmail.com>
2021-02-26 15:08:02 +0100

    fixed path in conservation

--- scripts/conserved_stall_sites.py
+++ scripts/conserved_stall_sites.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 
-### find conserved stall sites in different organisms
+''' Find conserved stall sites in different organisms. '''
 
 import os.path
 import gzip
 
-import cPickle as pickle
+import _pickle as pickle
 import subprocess
 from itertools import combinations
 
 # get GWP
 # library[stage][transcript] = [peak1, peak2...]
-#path = '/Volumes/USELESS/STALLING/conservation'
-path = '/export/valenfs/projects/kasia/STALLING'
+path = '../DATA'
 organisms = ['yeast', 'fruitfly', 'zebrafish', 'mouse', 'human']
 # os.path.join('/my/root/directory', 'in', 'here')
 
 
 gwpeaks = {}
 for org in organisms:
-	f = open(os.path.join(path, 'consensus_df/all_threshold', org+'.csv'), 'r')
+	f = open(os.path.join(path, 'all_peaks', org+'.csv'), 'r')
 	gwpeaks[org] = {}
 	for line in f:
 		tx = eval(line.split(',')[1])
@@ -32,14 +31,13 @@ for org in organisms:
 
 
 
-
 def readBiomart(organisms):
 	'''Takes a list of organism names, reads BioMart CSV files to get gene names. \
 	   Returns a dictionary of organisms `ens_gname[organism][gene_name] = list_of_transcript_ids`. '''
 	ens_gname = {}
 	for org in organisms:
 		ens_gname[org] = {}
-		export = open('/export/valenfs/projects/kasia/STALLING/biomart_export/'+org+'.txt', 'r')
+		export = open(os.path.join(path, 'biomart_export', org+'.txt'), 'r')
 		for line in export:
 			line = line.strip()
 			tx = line.split(',')[0]
@@ -55,7 +53,7 @@ def readBiomart(organisms):
 
 ens_gname = readBiomart(organisms[1:])
 ens_gname['yeast'] = {}
-export = open('/export/valenfs/projects/kasia/STALLING/biomart_export/yeast.txt', 'r')
+export = open(os.path.join(path, 'biomart_export', 'yeast.txt'), 'r')
 for line in export:
 	line = line.strip()
 	tx = line.split(',')[0]
@@ -126,16 +124,16 @@ def get_FASTA_sequence(filepath):
 
 
 fasta = {}
-fasta['yeast'] = get_FASTA_sequence('/Home/ii/katchyz/DATA/fasta/saccharomyces_cerevisiae/cds/Saccharomyces_cerevisiae.R64-1-1.cds.all.fa.gz')
-fasta['fruitfly'] = get_FASTA_sequence('/Home/ii/katchyz/DATA/fasta/drosophila_melanogaster/cds/Drosophila_melanogaster.BDGP6.cds.all.fa.gz')
-fasta['zebrafish'] = get_FASTA_sequence('/Home/ii/katchyz/DATA/fasta/danio_rerio/GRCz10/cds/Danio_rerio.GRCz10.cds.all.fa.gz')
+fasta['yeast'] = get_FASTA_sequence(os.path.join(path, 'fasta/saccharomyces_cerevisiae/cds/Saccharomyces_cerevisiae.R64-1-1.cds.all.fa.gz'))
+fasta['fruitfly'] = get_FASTA_sequence(os.path.join(path, 'fasta/drosophila_melanogaster/cds/Drosophila_melanogaster.BDGP6.cds.all.fa.gz'))
+fasta['zebrafish'] = get_FASTA_sequence(os.path.join(path, 'fasta/danio_rerio/GRCz10/cds/Danio_rerio.GRCz10.cds.all.fa.gz'))
 # names(fasta_cdna) <- sapply(names(fasta_cdna), function(x){substr(x,1,18)})
 for key in fasta['zebrafish'].keys():
 	fasta['zebrafish'][key[0:18]] = fasta['zebrafish'][key]
 	del fasta['zebrafish'][key]
 
-fasta['mouse'] = get_FASTA_sequence('/Home/ii/katchyz/DATA/fasta/mus_musculus/cds/Mus_musculus.GRCm38.cds.all.fa.gz')
-fasta['human'] = get_FASTA_sequence('/Home/ii/katchyz/DATA/fasta/homo_sapiens/cds/Homo_sapiens.GRCh38.cds.all.fa.gz')
+fasta['mouse'] = get_FASTA_sequence(os.path.join(path, 'fasta/mus_musculus/cds/Mus_musculus.GRCm38.cds.all.fa.gz'))
+fasta['human'] = get_FASTA_sequence(os.path.join(path, 'fasta/homo_sapiens/cds/Homo_sapiens.GRCh38.cds.all.fa.gz'))
 
 
 
@@ -146,7 +144,7 @@ lostSS = {}
 
 for n in range(len(organisms), 1, -1):
 	for curr_org_list in combinations(organisms, n):
-		print 'gene present in: ', curr_org_list
+		print('gene present in: ', curr_org_list)
 		### GET GENES IN COMMON
 		gene_tr_list = [ens_gname[org] for org in curr_org_list] # is it in organisms or curr_org_list?
 		genes_in_common = set.intersection(*map(set, gene_tr_list)) # set
@@ -169,6 +167,9 @@ for n in range(len(organisms), 1, -1):
 			homologs.close()
 			### run ClustalO
 			clustalo = subprocess.Popen(['clustalo', '-i', 'homologs.fasta'], stdout=subprocess.PIPE)
+			# to save alignment files, uncomment the following 2 lines (and add a directory to save aln files)
+			# out_file_name = gene+'_'+'_'.join(curr_org_list)+'.txt'
+			# subprocess.Popen(['clustalo', '-i', 'homologs.fasta', '-o', out_file_name])
 			alignment = parse_clustalo(clustalo)
 			### get absolute and relative peaks
 			abs_peaks = {}
@@ -183,7 +184,7 @@ for n in range(len(organisms), 1, -1):
 			### GET STALL SITES IN COMMON
 			for n2 in range(len(curr_org_list), 1, -1):
 				for curr_org_list2 in combinations(curr_org_list, n2):
-					print 'stall site conserved in: ', curr_org_list2
+					print('stall site conserved in: ', curr_org_list2)
 					############# GET CODE FOR PEAKS FROM BELOW HERE !!!!
 					abs_peaks_list = [(abs_peaks[org], org) for org in curr_org_list2]
 					for peak in abs_peaks_list[0][0]:
@@ -199,8 +200,8 @@ for n in range(len(organisms), 1, -1):
 									indices[abs_peaks_list[i][1]] = abs_peaks[abs_peaks_list[i][1]].index(min(ps, key=lambda x:abs(x-peak)))
 									conSS[gene][str(peak)].append([abs_peaks_list[i][1], rel_peaks[abs_peaks_list[i][1]][indices[abs_peaks_list[i][1]]]]) ##### !!!!!!!!!!
 						if len(indices) == len(abs_peaks_list):
-							print 'have a match:', peak # + DEL PEAK
-							print [(org, rel_peaks[org][indices[org]][0]) for org in indices]
+							print('have a match:', peak) # + DEL PEAK
+							print([(org, rel_peaks[org][indices[org]][0]) for org in indices])
 							for org in abs_peaks:
 								if org in indices:
 									del abs_peaks[org][indices[org]]
@@ -227,8 +228,8 @@ for n in range(len(organisms), 1, -1):
 
 
 ###
-pickle.dump(conSS, gzip.open('/export/valenfs/projects/kasia/STALLING/all_threshold/conSS.p.gz', 'wb'))
-pickle.dump(lostSS, gzip.open('/export/valenfs/projects/kasia/STALLING/all_threshold/lostSS.p.gz', 'wb'))
+pickle.dump(conSS, gzip.open(os.path.join(path, 'conSS.p.gz'), 'wb'))
+pickle.dump(lostSS, gzip.open(os.path.join(path, 'lostSS.p.gz'), 'wb'))
 
 
 ### get sequences around stall sites
@@ -241,7 +242,7 @@ for gene in conSS:
 		for org in conSS[gene][peak]:
 			conserved_stall_sites[gene][peak][org[0]] = org[1]
 
-pickle.dump(conserved_stall_sites, gzip.open('/export/valenfs/projects/kasia/STALLING/all_threshold/conserved_stall_sites.p.gz', 'wb'))
+pickle.dump(conserved_stall_sites, gzip.open(os.path.join(path, 'conserved_stall_sites.p.gz'), 'wb'))
 
 lost_stall_sites = {}
 for gene in lostSS:
@@ -251,48 +252,7 @@ for gene in lostSS:
 		for org_tr in lostSS[gene][peak]:
 			lost_stall_sites[gene][peak][org_tr[0]] = org_tr[1]
 
-pickle.dump(lost_stall_sites, gzip.open('/export/valenfs/projects/kasia/STALLING/all_threshold/lost_stall_sites.p.gz', 'wb'))
-
-
-css = conserved_stall_sites
-lss = lost_stall_sites
-
-c = 0
-for g in css:
-	for p in css[g]:
-		if 'yeast' in css[g][p] and 'fruitfly' in css[g][p] and 'zebrafish' in css[g][p] and 'mouse' in css[g][p] and 'human' in css[g][p]:
-			c += 1
-
-
-
-
-##########
-# get sequence
-css_logo = open('/export/valenfs/projects/kasia/STALLING/all_threshold/css_logo.txt', 'w')
-for gene in css:
-	for peak in css[gene]:
-		for org in css[gene][peak]:
-			ss = css[gene][peak][org][0]
-			tr = css[gene][peak][org][1]
-			logo = fasta[org][tr][ss-15:ss+18]
-			if len(logo) == 33:
-				css_logo.write(logo+'\n')
-
-css_logo.close()
-
-
-lss_logo = open('/export/valenfs/projects/kasia/STALLING/all_threshold/lss_logo.txt', 'w')
-for gene in lss:
-	for peak in lss[gene]:
-		for org in lss[gene][peak]:
-			ss = lss[gene][peak][org][0]
-			tr = lss[gene][peak][org][1]
-			logo = fasta[org][tr][ss-15:ss+18]
-			if len(logo) == 33:
-				lss_logo.write(logo+'\n')
-
-lss_logo.close()
-## do it on absolute alignment - see if there's any deletions etc.
+pickle.dump(lost_stall_sites, gzip.open(os.path.join(path, 'lost_stall_sites.p.gz'), 'wb'))
 
 
 
